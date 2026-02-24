61a7d3ef237a4cd0e5bf6fa43ec8602ab9ee6c4f
Decimals6 <aldo.rede9@gmail.com>
2026-02-24 08:44:57 +0700

    ganti vite untuk run local

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true,
+        hmr: {
+            host: '192.168.18.75', // ← ganti IP kamu
+        },
+    },
 });
