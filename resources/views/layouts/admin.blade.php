05c6f2f224763a6ab91915094da45c2a1c0242fa
Decimals6 <aldo.rede9@gmail.com>
2026-03-17 13:24:48 +0700

    merge

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Admin PCS DEV</title>
+    <title>Admin PCS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     {{-- logo tab browser --}}
